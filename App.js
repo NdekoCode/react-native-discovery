9344f65108aede16b37e3eaa3e48f43fac57569f
Ndekocode <arickbulakali@gmail.com>
2024-02-16 10:15:57 +0200

    refactor: test auto-correction text input propertie

--- App.js
+++ App.js
@@ -9,10 +9,10 @@ export default function App() {
   const [count, increment] = useCounter();
   return (
     <View style={styles.containerShadow}>
-      <TextInput placeholder="Entrer votre nom" style={styles.input}/>
+      <TextInput placeholder="Entrer votre nom" style={styles.input} auto-cordrection={false}/>
       <Text style={styles.text}>Hello Arick Bulakali {count}</Text>
       <Button title={`Increment: ${count}`} onPress={increment} />
-      <Compter initialValue={5} step={2}>Hello</Compter>
+      <Compter initialValue={5} step={2}>Hell</Compter>
       <UserComp/>
       <StatusBar style="auto" />
     </View>
