801c3d36bd3d99f35ec9b1a2d85175587b3c23c9
Ndekocode <arickbulakali@gmail.com>
2024-02-26 09:46:38 +0200

    learn keyboard component with his events

--- components/TextInputProps.js
+++ components/TextInputProps.js
@@ -1,9 +1,12 @@
-import { Button, TextInput, View } from "react-native"
+import { Button, TextInput, View ,Keyboard} from "react-native"
 import { appStyles } from "../styles/app"
 
 export const TextInputProps = ()=>{
+    const onHandlePress = ()=>{
+        Keyboard.dismiss();
+    }
     return <View style={appStyles.inputContainer}>
-        <TextInput style={appStyles.textInput} placeholder="Enter your new product" secureTextEntry={true}/>
-        <Button title="Valider"/>
+        <TextInput style={appStyles.textInput} placeholder="Enter your new product" secureTextEntry={true} onSubmitEditing={Keyboard.dismiss}/>
+        <Button title="Valider" onPress={onHandlePress}/>
     </View>
 }
