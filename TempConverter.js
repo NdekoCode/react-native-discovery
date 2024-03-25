4ef84850df823fc0baef57b91400f9854ba16f46
Ndekocode <arickbulakali@gmail.com>
2024-03-25 20:16:54 +0200

    feat: add basic layout

--- TempConverter.js
+++ TempConverter.js
@@ -1,12 +1,45 @@
-import { View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { tempStyle } from "./styles/tempConverter.style";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+const tempCorresponding = {
+  Celcius: "F",
+  Faranheit: "C",
+};
 export const TempConverter = () => {
+  const onChangeText = (value) => {};
+  const handleUnityChange = () => {};
+  const celciusToFaranheit = (value) => {
+    return (value * 9) / 5 + 32;
+  };
+  const FahrenheitToCelcius = (value) => {
+    return (value - 32) / (9 / 5);
+  };
+  const handleConverter = () => {};
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={tempStyle.container}>
-        <View style={tempStyle.workspace}></View>
+      <SafeAreaView style={{ ...tempStyle.container }}>
+        <View style={tempStyle.workspace}>
+          <Text style={tempStyle.textOutput}>
+            {`0 ° ${tempCorresponding["Celcius"]}`}
+          </Text>
+          <View style={tempStyle.textInputContainer}>
+            <TextInput
+              keyboardType="numeric"
+              value={String(0)}
+              placeholder="Enter unity to Converter"
+              onChangeText={onChangeText}
+              style={tempStyle.textInput}
+            />
+            <Text style={tempStyle.textInputLabel}>° C</Text>
+          </View>
+          <TouchableOpacity
+            onPress={handleConverter}
+            style={tempStyle.buttonApp}
+          >
+            <Text style={tempStyle.buttonApp.text}>Convertir en Celcius</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- styles/tempConverter.style.js
+++ styles/tempConverter.style.js
@@ -2,13 +2,51 @@ import { StyleSheet } from "react-native";
 
 export const tempStyle = StyleSheet.create({
   container: {
-    backgroundColor: "yellow",
+    backgroundColor: "green",
     justifyContent: "center",
+    alignItems: "center",
     flex: 1,
   },
   workspace: {
     minHeight: 490,
-    width: "max-content",
-    backgroundColor: "blue",
+    minWidth: 350,
+    width: "85%",
+    justifyContent: "center",
+    alignItems: "center",
+    rowGap: 55,
+  },
+  textInputContainer: {
+    width: "100%",
+    flexDirection: "row",
+    borderRadius: 12,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  textInput: {
+    color: "#333",
+    padding: 15,
+    fontWeight: "500",
+  },
+  textInputLabel: {
+    fontSize: 25,
+    color: "#111",
+  },
+  buttonApp: {
+    backgroundColor: "#111",
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    padding: "10 20",
+    borderRadius: 99,
+    text: {
+      fontSize: 20,
+      color: "#fff",
+    },
+  },
+  textOutput: {
+    fontSize: 65,
+    color: "white",
+    fontWeight: "600",
   },
 });
